0ab388135f70383da93aa432ed73a82af465851d
DcConan19 <aniketsingh70923@gmail.com>
2021-03-17 01:39:34 +0530

    commit

--- samsung/cycle.cpp
+++ samsung/cycle.cpp
@@ -6,37 +6,61 @@ using namespace std;
 class Solution
 {
 public:
-    bool dfs(int src, vector<bool> &vis, vector<bool> &revis, vector<int> adj[])
+   int s,e;
+   vector<int> par;
+    bool dfs(int u, vector<int> & col , vector<int> adj[])
     {
-        if (!vis[src])
-        {
-            vis[src] = true;
-            revis[src] = true;
-            for (int i : adj[src])
-            {
-                if (!vis[i] && dfs(i, vis, revis, adj))
+        col[u]=1;
+        for(int v : adj[u]){
+            if(col[v]==0){
+                par[v]=u;
+                if(dfs(v,col,adj)){
                     return true;
-                else if (revis[i] == true)
-                    return true;
-            }
-        }
-        revis[src] = false;
-        return false;
-    }
-    bool isCyclic(int V, vector<int> adj[])
-    {
-        // code here
-        vector<bool> vis(V, false);
-        vector<bool> revis(V, false);
-        for (int i = 0; i < V; i++)
-        {
-            if (dfs(i, vis, revis, adj))
-            {
+                }
+            }else if(col[v]==1){
+                s=v;
+                e=u;
+                // cout<<s<<" "<<e<<endl;
                 return true;
             }
         }
-
+        col[u]=2;
         return false;
+    }
+    bool isCyclic(int v, vector<int> adj[])
+    {
+        // code here
+        vector<int> col(v,0);
+        par.assign(v,-1);
+        // for (int i = 1; i <= v; i++)
+        // {
+        //     cout << par[i] << " ";
+        // }
+        s=-1;
+        for(int i=0;i<v;i++){
+            if(col[i]==0&&dfs(i,col,adj)){
+                break;
+            }
+        }
+        if(s==-1){
+            return false;
+        }
+        vector<int> cycle;
+        // cout<<s<<" "<<e<<" "<<v<<endl;
+        // for(int i=1;i<=v;i++){
+        //     cout<<par[i]<<" ";
+        // }
+        for(int i = e ; i !=s ;i=par[i]){
+            cycle.push_back(i);
+        }
+        cycle.push_back(s);
+        reverse(cycle.begin(),cycle.end());
+        for(auto i: cycle){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        return true;
+
     }
 };
 

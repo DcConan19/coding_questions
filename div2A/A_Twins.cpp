301fca496b985fd7efb5efb46d76d25d87cb517c
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-01 02:23:00 +0530

    hehhe

--- div2A/A_Twins.cpp
+++ div2A/A_Twins.cpp
@@ -2,22 +2,6 @@
 using namespace std;
 #define ll long long int
 
-#define TRACE
-#ifdef TRACE
-#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
-template <typename Arg1>
-void __f(const char* name, Arg1&& arg1) {
-    cout << name << " : " << arg1 << endl;
-    //use cerr if u want to display at the bottom
-}
-template <typename Arg1, typename... Args>
-void __f(const char* names, Arg1&& arg1, Args&&... args) {
-    const char* comma = strchr(names + 1, ','); cout.write(names, comma - names) << " : " << arg1 << " | "; __f(comma + 1, args...);
-}
-#else
-#define trace(...)
-#endif
-// unordered_set<int> s;
 
 
 void solve()

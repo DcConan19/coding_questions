ae61650b9c15f0c4f21bf521fac62fa82ec0f406
dh00mk3tu <cyberhybird@hotmail.com>
2021-04-08 21:52:53 +0530

    epic

---
+++ A_Game_With_Sticks.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<n<<m;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1 ;
+    // cin >> t;
+    ll x = 1;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ CodeChef/World_Record.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+void solve()
+{
+    float k1,bruh,k2,k3,v,can;
+    can = (k1*k2*k3*v);
+    can = (float)100/can;
+    // bruh = (int)(can * 100 + .5);
+    // float(bruh)/100;
+    if (can > 9.58){
+        cout<<can<<endl;
+    }
+    else {
+        cout<<can<<endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t = 1 ;
+    cin >> t;
+    ll x = 1;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+    return 0;
+}
